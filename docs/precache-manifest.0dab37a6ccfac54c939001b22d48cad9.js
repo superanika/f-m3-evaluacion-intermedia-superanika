49fe595b8b0bc2f1ed9deb690754f0f30bdf1bf6
superanika <anamaria.romerogarcia@gmail.com>
2019-05-13 08:45:20 +0200

    create docs for github

---
+++ docs/precache-manifest.0dab37a6ccfac54c939001b22d48cad9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117af4368222a94ecf8338160ead754a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bc5c396a27c4a7cc8cf4",
+    "url": "./static/css/main.27bf4f33.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "./static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "bc5c396a27c4a7cc8cf4",
+    "url": "./static/js/main.a9d7a70a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
