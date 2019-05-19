dd415332cdb9572299d03ae67545cb67501606c8
superanika <anamaria.romerogarcia@gmail.com>
2019-05-19 09:34:54 +0200

    create docs

--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -5,7 +5,7 @@ class Pokemon extends React.Component {
     render () {
         return (
             <div className="card">
-              <img src={this.props.url} alt="" className="poke_imgage"/>
+              <img src={this.props.url} alt={this.props.name} className="poke_imgage"/>
                 <h2 className="poke_name">{this.props.name}</h2>
                 <ul className="types">
                   {this.props.types.map((item, index) => (<li key={index}>{item}</li>))}
